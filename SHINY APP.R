c507447695506e9b1313f2ff3aad8c55ddf80780
NKUNDIMFURA <147813674+NKUNDIMFURA@users.noreply.github.com>
2025-01-31 03:43:42 +0200

    Add files via upload

---
+++ SHINY APP.R
@@ -0,0 +1,7 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='outreachprojectprimaryschools',
+                          token='DDE0FF1644EE1C7E07D56CBD955DBAE6',
+                          secret='sVh4Cg6n3lgEGiLILw9iXXz+Zy4nAjhhl2aXWqby')
+
+library(rsconnect)
+rsconnect::deployApp('C:/Users/DELL/OneDrive/Desktop/MYAPP')
